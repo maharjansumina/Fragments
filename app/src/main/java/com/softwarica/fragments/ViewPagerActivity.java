47ad4ef64a7c1d0eea2f277e341dcb7d38dc3b4f
sumina <suminamaharjan098@gmail.com>
2019-12-17 10:40:17 +0545

    ViewPager Added with Coded.

---
+++ app/src/main/java/com/softwarica/fragments/ViewPagerActivity.java
@@ -0,0 +1,34 @@
+package com.softwarica.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.softwarica.fragments.fragments.FirstFragment;
+import com.softwarica.fragments.fragments.SecondFragment;
+
+public class ViewPagerActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pager);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabId);
+
+        tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFFFFF"));
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new FirstFragment(), "Sum");
+        viewPagerAdapter.addFragment(new SecondFragment(), "Area of Circle");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
